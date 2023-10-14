ef9ee58994de69b459c947695e48f6a22c9e3423
artemexex3000 <mrtrololoshka70@gmail.com>
2023-10-14 16:10:25 +0300

    Added comment section.

--- database/migrations/2023_10_14_114930_create_comments_table.php
+++ database/migrations/2023_10_14_114930_create_comments_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->comment();
+            $table->foreignId('post_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->text('body');
             $table->timestamps();
         });
     }
